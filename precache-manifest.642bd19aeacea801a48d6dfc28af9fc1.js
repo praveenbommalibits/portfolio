2e1e9fd2be01dbfa70a3de59fa865a2d0981d620
praveenbommalibits <2018ab04128@wilp.bits-pilani.ac.in>
2020-06-06 21:08:02 +0530

    Updates

---
+++ precache-manifest.642bd19aeacea801a48d6dfc28af9fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7cedf1ab237fdbc6263393ae290a156",
+    "url": "/praveen_insight/index.html"
+  },
+  {
+    "revision": "1c1f9515b86ce0609482",
+    "url": "/praveen_insight/static/css/main.87b4974f.chunk.css"
+  },
+  {
+    "revision": "2c68341cacc9378c95db",
+    "url": "/praveen_insight/static/js/2.653eeb3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/praveen_insight/static/js/2.653eeb3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c1f9515b86ce0609482",
+    "url": "/praveen_insight/static/js/main.8f4bf920.chunk.js"
+  },
+  {
+    "revision": "8cee8c26377671039b65",
+    "url": "/praveen_insight/static/js/runtime-main.8e41f26e.js"
+  }
+]);
